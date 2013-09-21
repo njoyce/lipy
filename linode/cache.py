70df4fd077813e8c2261ab2623f7ea69297eebb1
Nick Joyce <nick@boxdesign.co.uk>
2013-09-21 17:04:19 +0100

    Move the cache to a user level directory

--- linode/cache.py
+++ linode/cache.py
@@ -1,15 +1,38 @@
-import os
+import os.path
+import errno
 import json
 
 
-def write_to_cache(file_name, result):
-    if not os.path.exists('.cache'):
-        os.mkdir('.cache')
+cache_dir = os.path.abspath(os.path.expanduser('~/.lipy/cache'))
 
-    with open('.cache/{}'.format(file_name), 'wb') as fp:
+
+def get_cache_dir():
+    global cache_dir
+
+    try:
+        os.makedirs(cache_dir)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+
+        if not os.path.isdir(cache_dir):
+            raise
+
+    return cache_dir
+
+
+def get_cache_filename(file_name):
+    return os.path.join(
+        get_cache_dir(),
+        file_name
+    )
+
+
+def write_to_cache(file_name, result):
+    with open(get_cache_filename(file_name), 'wb') as fp:
         fp.write(json.dumps(result))
 
 
 def read_from_cache(name):
-    with open('.cache/{}'.format(name), 'rb') as fp:
+    with open(get_cache_filename(name), 'rb') as fp:
         return json.loads(fp.read())
